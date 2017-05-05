1b5597058e10d09b37f0f55b5d42b76eefc8fb14
Kay Christensen <kaychristensen@gmail.com>
2017-05-04 19:18:28 -0700

    Feature sum (#6)
    * merged the feature-sum branch to the main branch too early
    
    * deploy app to heroku
    
    * Kay's feedback #1: Use 2-space tabs instead of 4 and convert indentations to spaces
    
    * Delete bundle.js
    
    * Kay's feedback #2: Delete unnecessary comments and console logs
    
    * Kay's feedback #3: Add bundle.js to .gitignore
    
    * Kay's feedback #4: Replace all var's with let or const
    
    * Modify sum footer column to ignore non-numeric cells
    
    * simplify renderTableFooter function and create 3 tests
    
    * add bundle.js to .gitignore
    
    * last resort: can't push without bundle.js for some reason?
    
    * Delete bundle.js
    
    * Remove unnecessary comments
    
    * update .gitignore
    
    * After all of Kay's feedback except line 39 on table-view.test of which I'm unsure of

--- client/js/dom-util.js
+++ client/js/dom-util.js
@@ -1,17 +1,17 @@
 const removeChildren = function(parentEl) {
-	while (parentEl.firstChild) {
-		parentEl.removeChild(parentEl.firstChild);
-	}
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
 };
 
 const createEl = function(tagName) {
-	return function(text) {
-		const el = document.createElement(tagName);
-		if (text) {
-			el.textContent = text;
-		}
-		return el;
-	};
+  return function(text) {
+    const el = document.createElement(tagName);
+    if (text) {
+      el.textContent = text;
+    }
+    return el;
+  };
 };
 
 const createTH = createEl('TH');
@@ -19,8 +19,8 @@ const createTR = createEl('TR');
 const createTD = createEl('TD');
 
 module.exports = {
-	createTH: createTH,
-	createTR: createTR,
-	createTD: createTD,
-	removeChildren: removeChildren,
+  createTH: createTH,
+  createTR: createTR,
+  createTD: createTD,
+  removeChildren: removeChildren,
 }
--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -4,158 +4,205 @@ const TableView = require('../table-view');
 
 describe('table-view', () => {
 
-    beforeEach(() => {
-        // load HTML skeleton from disk and parse into the DOM
-        const fixturePath = './client/js/test/fixtures/sheet-container.html';
-        const html = fs.readFileSync(fixturePath, 'utf8');
-        document.documentElement.innerHTML = html;
+  beforeEach(() => {
+    // load HTML skeleton from disk and parse into the DOM
+    const fixturePath = './client/js/test/fixtures/sheet-container.html';
+    const html = fs.readFileSync(fixturePath, 'utf8');
+    document.documentElement.innerHTML = html;
+  });
+
+  describe('footer sum', () => {
+    it('returns the sum of positive numbers', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect initial state
+      let footerRow = document.querySelector('TFOOT TR');
+      expect(footerRow.cells[1].textContent).toBe('');
+
+      // simulate user action
+      model.setValue({ col: 1, row: 0 }, '2');
+      model.setValue({ col: 1, row: 1 }, '3');
+      model.setValue({ col: 1, row: 2 }, '5');
+
+      view.renderTableBody();
+      view.renderTableFooter();
+
+      // inspect the resulting action
+      let rows = document.querySelectorAll('TBODY TR');
+      expect(rows[1].cells[1].textContent).toBe('3');
+      view.renderTableFooter();
+
+      const footerCells = document.querySelectorAll('TFOOT TD');
+      expect(footerCells[1].textContent).toBe('10');
+
     });
 
-    describe('footer sum', () => {
-        it('returns the sum of all numbers in the same column', () => {
-            // set up the initial state
-            const model = new TableModel(3, 3);
-            const view = new TableView(model);
-            model.setValue({ col: 1, row: 1 }, '1');
-            model.setValue({ col: 1, row: 2 }, '2');
-            model.setValue({ col: 1, row: 3 }, '3');
-            model.setValue({ col: 2, row: 1 }, '4');
-            model.setValue({ col: 2, row: 2 }, '5');
-            model.setValue({ col: 2, row: 3 }, '6');
-            model.setValue({ col: 3, row: 1 }, '7');
-            model.setValue({ col: 3, row: 2 }, '8');
-            model.setValue({ col: 3, row: 3 }, '8');
-            view.init();
+    it('returns the sum of negative numbers', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
 
-            // inspect the initial state
-            let trs = document.querySelectorAll('TBODY TR');
-            expect(trs[1].cells[2].textContent).toBe('4');
+      // inspect initial state
+      let footerRow = document.querySelector('TFOOT TR');
+      expect(footerRow.cells[1].textContent).toBe('');
 
-            // simulate user action
-            let frs = document.querySelectorAll('TFOOT TR');
-            view.renderTableFooter();
+      // simulate user action
+      model.setValue({ col: 1, row: 0 }, '-2');
+      model.setValue({ col: 1, row: 1 }, '-3');
+      model.setValue({ col: 1, row: 2 }, '-5');
 
-            //  inspect the resulting action
-            expect(frs[1].cells[2].textContent).toBe('');
-            console.log(frs[2]);
+      view.renderTableBody();
+      view.renderTableFooter();
 
-        });
+      // inspect the resulting action
+      let rows = document.querySelectorAll('TBODY TR');
+      expect(rows[1].cells[1].textContent).toBe('-3');
+      view.renderTableFooter();
+
+      const footerCells = document.querySelectorAll('TFOOT TD');
+      expect(footerCells[1].textContent).toBe('-10');
+
     });
-    /* 
+
+    it('returns the sum of strings and numbers', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect initial state
+      let footerRow = document.querySelector('TFOOT TR');
+      expect(footerRow.cells[1].textContent).toBe('');
+
+      // simulate user action
+      model.setValue({ col: 1, row: 0 }, '-2');
+      model.setValue({ col: 1, row: 1 }, '3');
+      model.setValue({ col: 1, row: 2 }, 'hack-reactor');
+
+      view.renderTableBody();
+      view.renderTableFooter();
+
+      // inspect the resulting action
+      let rows = document.querySelectorAll('TBODY TR');
+      expect(rows[1].cells[1].textContent).toBe('3');
+      view.renderTableFooter();
+
+      const footerCells = document.querySelectorAll('TFOOT TD');
+      expect(footerCells[1].textContent).toBe('1');
+
+    });
+
+  });
+
+  describe('formula bar', () => {
+    it('makes changes To the value of the current cell', () => {
+      // set up the intitial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[0].cells[0];
+      expect(td.textContent).toBe('');
+
+      // simulate user action
+      document.querySelector('#formula-bar').value = '65';
+      view.handleFormulaBarChange();
+
+      // inspect the resulting action
+      trs = document.querySelectorAll('TBODY TR');
+      expect(trs[0].cells[0].textContent).toBe('65');
+    });
+
+    it('updates FROM the value of the current cell', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({ col: 2, row: 1 }, '123');
+      view.init();
+
+      // inspect the initial state
+      const formulaBarEl = document.querySelector('#formula-bar');
+      expect(formulaBarEl.value).toBe('');
+
+      // simulate user action
+      const trs = document.querySelectorAll('TBODY TR');
+      trs[1].cells[2].click();
+
+      // inspect the resulting state
+      expect(formulaBarEl.value).toBe('123');
+    });
+  });
+
+  describe('table body', () => {
+    it('highlights the current cell when clicked', () => {
+      // set up the initial state
+      const model = new TableModel(10, 5);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      // simulate user action
+      td.click();
+
+      // inspect the resulting action
+      trs = document.querySelectorAll('TBODY TR');
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    });
+
+    it('has the right size', () => {
+      // set up the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+    });
+
     it('fills in values from the model', () => {
-                // set up the initial state
-                const model = new TableModel(3, 3);
-                const view = new TableView(model);
-                model.setValue({col: 2, row: 1}, '123');
-                view.init();
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({ col: 2, row: 1 }, '123');
+      view.init();
 
-                // inspect the initial state
-                const trs = document.querySelectorAll('TBODY TR');
-                expect(trs[1].cells[2].textContent).toBe('123');
-            })*/
-    
-    describe('formula bar', () => {
-        it('makes changes To the value of the current cell', () => {
-            // set up the intitial state
-            const model = new TableModel(3, 3);
-            const view = new TableView(model);
-            view.init();
+      // inspect the initial state
+      const trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');
+    });
 
-            // inspect the initial state
-            let trs = document.querySelectorAll('TBODY TR');
-            let td = trs[0].cells[0];
-            expect(td.textContent).toBe('');
+  });
 
-            // simulate user action
-            document.querySelector('#formula-bar').value = '65';
-            view.handleFormulaBarChange();
+  describe('table header', () => {
+    it('has valid column header lablels', () => {
+      // set up the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
 
-            // inspect the resulting action
-            trs = document.querySelectorAll('TBODY TR');
-            expect(trs[0].cells[0].textContent).toBe('65');
-        })
-        it('updates FROM the value of the current cell', () => {
-            // set up the initial state
-            const model = new TableModel(3, 3);
-            const view = new TableView(model);
-            model.setValue({ col: 2, row: 1 }, '123');
-            view.init();
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
 
-            // inspect the initial state
-            const formulaBarEl = document.querySelector('#formula-bar');
-            expect(formulaBarEl.value).toBe('');
+      let labelTexts = Array.from(ths).map(el => el.textContent);
+      expect(labelTexts).toEqual(["A", "B", "C", "D", "E", "F"]);
+    });
 
-            // simulate user action
-            const trs = document.querySelectorAll('TBODY TR');
-            trs[1].cells[2].click();
-
-            // inspect the resulting state
-            expect(formulaBarEl.value).toBe('123');
-        })
-    })
-
-    describe('table body', () => {
-        it('highlights the current cell when clicked', () => {
-            // set up the initial state
-            const model = new TableModel(10, 5);
-            const view = new TableView(model);
-            view.init();
-
-            // inspect the initial state
-            let trs = document.querySelectorAll('TBODY TR');
-            let td = trs[2].cells[3];
-            expect(td.className).toBe('');
-
-            // simulate user action
-            td.click();
-
-            // inspect the resulting action
-            trs = document.querySelectorAll('TBODY TR');
-            td = trs[2].cells[3];
-            expect(td.className).not.toBe('');
-        });
-
-        it('has the right size', () => {
-            // set up the initial state
-            const numCols = 6;
-            const numRows = 10;
-            const model = new TableModel(numCols, numRows);
-            const view = new TableView(model);
-            view.init();
-
-            // inspect the initial state
-            let ths = document.querySelectorAll('THEAD TH');
-            expect(ths.length).toBe(numCols);
-        });
-
-        it('fills in values from the model', () => {
-            // set up the initial state
-            const model = new TableModel(3, 3);
-            const view = new TableView(model);
-            model.setValue({ col: 2, row: 1 }, '123');
-            view.init();
-
-            // inspect the initial state
-            const trs = document.querySelectorAll('TBODY TR');
-            expect(trs[1].cells[2].textContent).toBe('123');
-        })
-    })
-
-    describe('table header', () => {
-        it('has valid column header lablels', () => {
-            // set up the initial state
-            const numCols = 6;
-            const numRows = 10;
-            const model = new TableModel(numCols, numRows);
-            const view = new TableView(model);
-            view.init();
-
-            // inspect the initial state
-            let ths = document.querySelectorAll('THEAD TH');
-            expect(ths.length).toBe(numCols);
-
-            let labelTexts = Array.from(ths).map(el => el.textContent);
-            expect(labelTexts).toEqual(["A", "B", "C", "D", "E", "F"]);
-        })
-    })
-})
+  });
+});
